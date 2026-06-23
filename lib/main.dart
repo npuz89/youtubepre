5681343dd41db48a7a93a73908434af9eef80645
npuz89 <79442320+npuz89@users.noreply.github.com>
2026-06-23 07:23:01 +0000

    update 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,52 @@
+import 'dart:ui';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Включаем удержание процессора, чтобы музыка не спала
+  WakelockPlus.enable();
+  
+  // Инициализируем фоновый сервис Android
+  await initializeService();
+  
   runApp(const MyApp());
+}
+
+// Настройка фонового сервиса Android
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      onStart: onStart,
+      autoStart: true,
+      isForegroundMode: true, // Говорим Android, что это важное фоновое приложение
+      notificationChannelId: 'my_foreground',
+      initialNotificationTitle: 'YouTube Background Player',
+      initialNotificationContent: 'Приложение работает в фоновом режиме',
+      foregroundServiceTypes: [AndroidForegroundServiceType.mediaPlayback],
+    ),
+    iosConfiguration: IosConfiguration(),
+  );
+}
+
+@pragma('vm:entry-point')
+void onStart(ServiceInstance service) async {
+  DartPluginRegistrant.ensureInitialized();
+  
+  // Этот код просто держит службу активной в фоне
+  Timer.periodic(const Duration(seconds: 1), (timer) async {
+    if (service is AndroidServiceInstance) {
+      if (await service.isForegroundService()) {
+        service.setAsForegroundService();
+      }
+    }
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -24,10 +67,19 @@ class YouTubeScreen extends StatefulWidget {
   const YouTubeScreen({super.key});
 
   @override
-  State<YouTubeScreen> createState() => _YouTubeScreenState();
+  Widget build(BuildContext context) {
+    return const YouTubeScreenStateful();
+  }
 }
 
-class _YouTubeScreenState extends State<YouTubeScreen> {
+class YouTubeScreenStateful extends StatefulWidget {
+  const YouTubeScreenStateful({super.key});
+
+  @override
+  State<YouTubeScreenStateful> createState() => _YouTubeScreenState();
+}
+
+class _YouTubeScreenState extends State<YouTubeScreenStateful> {
   late final WebViewController _controller;
 
   @override
@@ -44,6 +96,7 @@ class _YouTubeScreenState extends State<YouTubeScreen> {
       ..setNavigationDelegate(
         NavigationDelegate(
           onPageFinished: (String url) {
+            // Обман плеера YouTube, чтобы он думал, что вкладка активна
             _controller.runJavaScript('''
               Object.defineProperty(document, 'hidden', {get: function() { return false; }, configurable: true});
               Object.defineProperty(document, 'visibilityState', {get: function() { return 'visible'; }, configurable: true});
